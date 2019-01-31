8ecb1b007376bfbdba35867a3c81d18860197f3a
Diego Valadez <the-vala@users.noreply.github.com>
2019-01-30 20:37:35 -0600

    Corrected matrixHandler

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from classes.Lexical import Lexical
 def main():
     # file = open("file.txt", "r")
     # lineas = file.readlines()
-    
+    lineas = "15 5 $vala + 4 - * / $er4 $4rt "
     # print(lineas)
     # file.close()
-    linea = input()
+    # linea = input()
     # print(len(linea))
     l = Lexical()
+    tokens = l.matrixHandler(lineas)
+    for token in tokens:
+        print(token)
+    
+
 
 
   
