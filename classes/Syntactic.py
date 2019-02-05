091f70e04b9be690d96b701546f98c4c72696faa
Diego Valadez <the-vala@users.noreply.github.com>
2019-02-05 12:32:42 -0600

    Created recursiveDescent

--- classes/Syntactic.py
+++ classes/Syntactic.py
@@ -1,3 +1,27 @@
+from .Lexical import Token
+
 class Syntactic:
-    def __init__(self, tokens):
-        self.tokensList = tokens
+    def __init__(self):
+        self.tokenList = []
+
+    def syntacticAnalysis(self, string):
+        l = Lexical()
+        self.tokensList = l.matrixHandler(string)
+
+        if (self.recursiveDescent()):
+            print("The input es a well formed expression.\n")
+        else:
+            print("SYNTACTIC ERROR: The expression is not valid.\n")
+
+    def recursiveDescent(self):
+        valid = true
+        valid = match("(", self.tokensList) #create match
+        if (valid):
+            valid = parseBody(self.tokensList)
+        if (valid):
+            valid = match(")", self.tokensList)
+        return valid
+
+    def match ():
+        #do some shit
+        self.tokenList = self.tokenList[1:]
