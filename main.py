080e50fc8db86b8dfd8e56a3e728029e3036f529
Diego Valadez <the-vala@users.noreply.github.com>
2019-02-08 13:37:52 -0600

    Message promted

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+#Test case 1: (+ 4 10 )  Correct
+#Test case 1: (+ 4 10)   Lexical error
+#Test case 1: (+ 10 )    Syntactic error
+#Test case 1: (+ 4 $vala )  Correct
+
 from classes.Lexical import Lexical
 from classes.Syntactic import Syntactic
 def main():
+    print ("String? ", end = '')
     linea = input()
     l = Lexical()
     tokenList, tokenValid = l.matrixHandler(linea)
